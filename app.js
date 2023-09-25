74d788eeb91741576826df7dec8625757b6c1af7
Kim Weast <weastkim3@gmail.com>
2023-09-25 16:59:44 -0500

    update work

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
-console.log("Hello World!\n==========\n");
+//Exercise 1
+// 1Exercise 1 Section=========
 
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+const myButton = document.getElementById("myButton");
+myButton.addEventListener("click", function() {
+    alert("Button clicked!");
+});
+const searchTerm = searchInPut.value.trim();
+const myImage = document.getElementById("myImage");
+const showFeedbackParagraph = getElementById("showParagraph")
+
+
+        
+    searchBttn.addEventListener("click", () => { 
+    fetch( `https://api.giphy.com/v1/gifs/translate?myapi_key=${myapiKey}&s=${encodeURIComponen(searchTerm)}`)
+
+    .then(response => response.json() )
+
+    //exercise 4
+    .then(jsonData => {
+        const imageElement = searchResults.querySelector("img");
+        if (imageElement) {
+            imageElement.src = jsonData.data.images.original.url;
+            searchInput.value = '';
+        }
+    } )
+    
+});
