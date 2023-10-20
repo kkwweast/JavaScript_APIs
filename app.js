f149155baafbcf6f552c70ef1b06d8c4cd427b8f
Kim Weast <weastkim3@gmail.com>
2023-10-20 14:43:59 -0500

    update

--- app.js
+++ app.js
@@ -1,28 +1,70 @@
-//Exercise 1
-// 1Exercise 1 Section=========
-
-const myButton = document.getElementById("myButton");
-myButton.addEventListener("click", function() {
-    alert("Button clicked!");
-});
-const searchTerm = searchInPut.value.trim();
-const myImage = document.getElementById("myImage");
-const showFeedbackParagraph = getElementById("showParagraph")
 
 
-        
-    searchBttn.addEventListener("click", () => { 
-    fetch( `https://api.giphy.com/v1/gifs/translate?myapi_key=${myapiKey}&s=${encodeURIComponen(searchTerm)}`)
 
-    .then(response => response.json() )
 
-    //exercise 4
-    .then(jsonData => {
-        const imageElement = searchResults.querySelector("img");
-        if (imageElement) {
-            imageElement.src = jsonData.data.images.original.url;
-            searchInput.value = '';
-        }
-    } )
+
+//This is my giphy API key
+const APIKEY = "http://api.openweathermap.org/data/2.5/weather";
+
+console.log("Hello Fetch API!");
+
+//query paramets follow a ?
+//name=valu pairs
+//each pair seperated by a &
+const WEATHER_URL ="https://api.openweathermap.org/data/2.5/weather";
+
+const QUERY_PARAMS = "?quhoover=&a5a839a780c1d61b9710aca75a4e11fa&units=imperial";
+
+
+
+fetch(WEATHER_URL + QUERY_PARAMS);
+
+    .then((response) => {
+        return response.json();
+    })
+    .then((weather) => {
+        console.log(weather);
+        //do anything with the weather data
+        document.querySelector(".title").textContent =
+    })
+    .catch((err) => console.error(err));
+
+    function displayWeather(weather) {
+        document.querySelector(
+            ".title"
+           ) .textContent = `It is currently ${weather.main.temp} in ${wather.name}
+        });
+
+ async function getWeather() {
+       try{
+        let response = await fetch(WEATHER_URL + QERY_PARAMS);
+        let weather = await response.json();
+         displayWeather(weather);
+       }catch(err) {
+        console.error(err);
+       }
+    }
+
+
+
+
+       }
+    }
+
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           `
+    }
     
-});
+        
+    
+    
